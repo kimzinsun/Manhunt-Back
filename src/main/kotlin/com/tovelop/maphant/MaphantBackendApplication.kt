86eca5d43f4c8289250c331dce0171ae668066bb
HanDaWoon <bookpia.han@gmail.com>
2023-07-03 20:25:49 +0900

    Feat : 프로젝트 생성 및 기본 설정
    .gitignore 추가 property 파일 등록
    application.properties mybatis 설정 및 database,redis 설정 포함

---
+++ src/main/kotlin/com/tovelop/maphant/MaphantBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.tovelop.maphant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MaphantBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MaphantBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/tovelop/maphant/MaphantBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tovelop.maphant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MaphantBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
