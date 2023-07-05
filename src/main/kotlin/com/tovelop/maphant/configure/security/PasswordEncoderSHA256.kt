9f81bf8f7b2f3491af70da899f5c801052e14c0b
kou466 <1226hajing@gmail.com>
2023-07-05 16:44:23 +0900

    PasswordEncoderSHA256.kt 중복된 기존 fun 삭제

--- src/main/kotlin/com/tovelop/maphant/configure/security/PasswordEncoderSHA256.kt
+++ src/main/kotlin/com/tovelop/maphant/configure/security/PasswordEncoderSHA256.kt
@@ -6,12 +6,6 @@ import java.security.MessageDigest
 
 @Component
 class PasswordEncoderSHA256 : PasswordEncoder {
-    fun SHA256(pw: String): String {
-        val bytes = pw.toByteArray()
-        val md = MessageDigest.getInstance("SHA-256")
-        val digest = md.digest(bytes)
-        return digest.fold("") { str, it -> str + "%02x".format(it) }
-    }
 
     override fun encode(rawPassword: CharSequence): String {
         val digest = MessageDigest.getInstance("SHA-256")
