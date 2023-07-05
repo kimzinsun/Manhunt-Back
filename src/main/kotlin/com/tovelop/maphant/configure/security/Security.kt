1909f9169f39bd86a82f81832409c56821930719
uomaep <uomaep@gmail.com>
2023-07-05 17:58:49 +0900

    feat: any request permit all

--- src/main/kotlin/com/tovelop/maphant/configure/security/Security.kt
+++ src/main/kotlin/com/tovelop/maphant/configure/security/Security.kt
@@ -2,6 +2,8 @@ package com.tovelop.maphant.configure.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -10,9 +12,16 @@ import org.springframework.security.web.SecurityFilterChain
 @Configuration
 @EnableWebSecurity
 class Security {
+    lateinit var manager: AuthenticationManager
+
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain? {
-        http.csrf { it.disable() }
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests { autorize ->
+                autorize.anyRequest().permitAll()
+            }
         return http.build()
     }
+
 }
