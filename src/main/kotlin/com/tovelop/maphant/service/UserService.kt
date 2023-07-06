8791fbde34887835eeef99a998dcdd0cd43e11bd
alscjf1329 <alscjf1329@gmail.com>
2023-07-06 11:17:05 +0900

    Feat: check User UK exist

--- src/main/kotlin/com/tovelop/maphant/service/UserService.kt
+++ src/main/kotlin/com/tovelop/maphant/service/UserService.kt
@@ -5,9 +5,17 @@ import org.springframework.stereotype.Service
 
 
 @Service
-class   UserService(val mapper: UserMapper) {
-    fun existSameEmail(email:String):Boolean{
+class UserService(val mapper: UserMapper) {
+    fun existSameEmail(email: String): Boolean {
         return mapper.countSameEmails(email) != 0
+    }
+
+    fun existSameNickName(nickName: String): Boolean {
+        return mapper.countSameNickName(nickName) != 0
+    }
+
+    fun existSamePhoneInt(phoneInt: String): Boolean {
+        return mapper.countSamePhoneInt(phoneInt) != 0
     }
 
 }
