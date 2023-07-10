7fec0f534153bcdc5eac8040ae9424410a95812e
cottonrose1011 <pjeka7er43@gmail.com>
2023-07-10 16:58:43 +0900

    Add LoginAuthProvider

---
+++ src/main/kotlin/com/tovelop/maphant/configure/security/SecurityLoginAuthProvider.kt
@@ -0,0 +1,37 @@
+package com.tovelop.maphant.configure.security
+
+import com.tovelop.maphant.configure.MockupCustomUserService
+import com.tovelop.maphant.configure.MockupCustomUserToken
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+
+@Component
+class SecurityLoginAuthProvider(
+    @Autowired val userDataService: MockupCustomUserService,
+    @Autowired val tokenRepo: SecurityTokenRepository
+) : AuthenticationProvider {
+    override fun authenticate(authentication: Authentication?): Authentication {
+        if (authentication == null) {
+            throw SecurityException("No authentication")
+        }
+
+        val usernamePasswordAuthenticationToken = authentication as MockupCustomUserToken
+        val userData = userDataService.loadUserByUsername(usernamePasswordAuthenticationToken.name)
+
+        if (userData.password != usernamePasswordAuthenticationToken.credentials) {
+            throw BadCredentialsException("Invalid Password")
+        }
+
+        val auth = tokenRepo.generateToken(userData)
+        return SecurityLoginAuthToken(auth.first, auth.second, userData)
+    }
+
+    override fun supports(authentication: Class<*>?): Boolean {
+        if (authentication == null) return false
+
+        return authentication == MockupCustomUserToken::class.java
+    }
+}
