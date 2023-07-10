a440fd0a8cf6cf38e48d7879d45ab941fa6f350d
LeeJaeHoon <dlwogns3413@naver.com>
2023-07-10 17:06:26 +0900

    Feat: SMS 인증코드 문자 발송 완료

---
+++ src/main/kotlin/com/tovelop/maphant/dto/SmsResponseDto.kt
@@ -0,0 +1,10 @@
+package com.tovelop.maphant.dto
+
+class SmsResponseDto {
+    lateinit var requestId: String
+    lateinit var requestTime: String
+    lateinit var statusCode: String
+    lateinit var statusName: String
+}
+
+
