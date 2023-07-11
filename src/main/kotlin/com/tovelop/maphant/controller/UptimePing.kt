a8bbf3a4db1fd8fe8bcc3f46eac365a92fe75897
ESukmean <esukmean@esukmean.com>
2023-07-11 16:13:01 +0900

    jenkins 체크용 endpoint 추가

--- src/main/kotlin/com/tovelop/maphant/controller/UptimePing.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/UptimePing.kt
@@ -1,20 +1,18 @@
 package com.tovelop.maphant.controller
 
-import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 @RequestMapping("/ping")
 class UptimePing {
-    @RequestMapping("/check")
-    @ResponseBody
+    @GetMapping("/check")
     fun ping(): String {
         return "pong"
     }
 
-    @RequestMapping("/jenkins")
-    @ResponseBody
+    @GetMapping("/jenkins")
     fun jenkins(): String {
         return "pong"
     }
