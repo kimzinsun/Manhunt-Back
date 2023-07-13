05fcd766cc24fa58a670ffcde3eb98577e0fa6af
alscjf1329 <alscjf1329@gmail.com>
2023-07-13 15:45:50 +0900

    Refactor) isValidatedEmail으로 변경

--- src/main/kotlin/com/tovelop/maphant/service/UserService.kt
+++ src/main/kotlin/com/tovelop/maphant/service/UserService.kt
@@ -10,7 +10,7 @@ import java.time.LocalDate
 
 @Service
 class UserService(val mapper: UserMapper) {
-    fun findStateByUserId(userId: Int) = mapper.findStateByUserId(userId)
+    fun isValidatedEmail(userId: Int) = mapper.findStateByUserId(userId) == '1'
     fun findPasswordByEmail(email: String) = mapper.findPasswordByEmail(email)
 
     fun updateUserState(email: String, state: Char) {
