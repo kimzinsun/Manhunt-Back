8c753154bd6b034f57f5788f84de172aa3a70e3e
alscjf1329 <alscjf1329@gmail.com>
2023-07-13 14:06:22 +0900

    Refactor) findPostById return 변경

--- src/main/kotlin/com/tovelop/maphant/service/BoardService.kt
+++ src/main/kotlin/com/tovelop/maphant/service/BoardService.kt
@@ -4,21 +4,20 @@ import com.tovelop.maphant.dto.BoardDTO
 import com.tovelop.maphant.mapper.BoardMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 
 @Service
 class BoardService(@Autowired val boardMapper: BoardMapper) {
     fun createPost(boardDTO: BoardDTO) {
-//        val board = BoardDTO()
-//        return boardMapper.createPost(board)
-    }
-
-    fun readPost(postId: Int) {
-
 
     }
 
-    fun updatePost(boardDTO: BoardDTO) {
+    fun findPostById(postId: Int):BoardDTO {
+        return BoardDTO(1,1,1,"asdfl","asdf","asdf","asdf",true, LocalDateTime.now(), LocalDateTime.now())
+    }
+
+    fun updatePost(boardDTO: BoardDTO){
         val postId = boardDTO.postId
 
 
