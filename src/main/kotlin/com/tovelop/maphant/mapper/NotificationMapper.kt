a106b1907f25f8b243a9b2e9bbab9fc6d4de6181
kou466 <1226hajing@gmail.com>
2023-07-17 17:12:26 +0900

    DTO 변경

--- src/main/kotlin/com/tovelop/maphant/mapper/NotificationMapper.kt
+++ src/main/kotlin/com/tovelop/maphant/mapper/NotificationMapper.kt
@@ -1,13 +1,13 @@
 package com.tovelop.maphant.mapper
 
 import com.tovelop.maphant.dto.FcmMessageDTO
-import com.tovelop.maphant.dto.NotificationResponseDTO
+import com.tovelop.maphant.dto.NotificationDBDTO
 import org.apache.ibatis.annotations.Mapper
 import org.springframework.stereotype.Repository
 
 @Mapper
 @Repository
 interface NotificationMapper {
-    fun getNotificationsByUserId(userId: Int): List<NotificationResponseDTO>
+    fun getNotificationsByUserId(userId: Int): List<NotificationDBDTO>
     fun createNotification(notification: FcmMessageDTO, jsonStr: String)
 }
