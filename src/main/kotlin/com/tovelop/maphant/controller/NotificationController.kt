025e59d0a3b2475750d46642d58ce86efdc15502
kou466 <1226hajing@gmail.com>
2023-07-17 17:18:59 +0900

    불러온 알림을 새로운 DTO객체로 변환시키는 코드 추가

--- src/main/kotlin/com/tovelop/maphant/controller/NotificationController.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/NotificationController.kt
@@ -1,8 +1,6 @@
 package com.tovelop.maphant.controller
 
-import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.tovelop.maphant.dto.NotificationResponseDTO
 import com.tovelop.maphant.service.NotificationService
 import org.springframework.web.bind.annotation.*
@@ -10,10 +8,17 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/notifications")
 class NotificationController(private val notificationService: NotificationService) {
-    private val objectMapper = ObjectMapper()
 
     @GetMapping("/{userId}")
     fun getNotificationsByUserId(@PathVariable userId: Int): List<NotificationResponseDTO> {
-        return notificationService.getNotificationsByUserId(userId)
+        return notificationService.getNotificationsByUserId(userId).map {
+            NotificationResponseDTO(
+                id = it.id,
+                etc = ObjectMapper().readValue(it.etc, Map::class.java),
+                title = it.title,
+                body = it.body,
+                createdAt = it.createdAt
+            )
+        }
     }
 }
