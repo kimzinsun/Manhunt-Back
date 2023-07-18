a196827da66404d9f372aa1d5ceb76b285f7a7b3
pwrwpw <dlgustjr9912@gmail.com>
2023-07-18 14:49:17 +0900

    BoardDTO 수정

--- src/main/kotlin/com/tovelop/maphant/mapper/BoardMapper.kt
+++ src/main/kotlin/com/tovelop/maphant/mapper/BoardMapper.kt
@@ -3,13 +3,17 @@ package com.tovelop.maphant.mapper
 import com.tovelop.maphant.dto.BoardDTO
 import org.apache.ibatis.annotations.Mapper
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
 @Mapper
 @Repository
-class BoardMapper {
-    fun createPost(boardDTO: BoardDTO) {}
-    fun readPost(boardDTO: BoardDTO) {}
-    fun updatePost(boardDTO: BoardDTO) {}
-    fun deletePost(boardDTO: BoardDTO) {}
+interface BoardMapper {
+    fun insertBoard(board: BoardDTO)
+    fun updateBoard(board: BoardDTO)
+    fun deleteBoard(boardId: Int)
+    fun getBoardById(boardId: Int): BoardDTO?
+    fun getAllBoards(): List<BoardDTO>
+    fun getBoardsByCategoryId(categoryId: Int): List<BoardDTO>
+    fun getBoardsByUserId(userId: Int): List<BoardDTO>
 
 }
