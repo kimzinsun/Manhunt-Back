605c3066110f0987d7b4b125112166de1fb1b8c2
ESukmean <esukmean@esukmean.com>
2023-07-18 21:54:15 +0900

    SMS 전송 모듈을 좀 더 범용적으로 수정하고, 인증 코드 발송 코드 보강

--- src/main/kotlin/com/tovelop/maphant/controller/SmsController.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/SmsController.kt
@@ -1,39 +1,39 @@
 package com.tovelop.maphant.controller
 
-import com.tovelop.maphant.dto.MessageDto
-import com.tovelop.maphant.dto.SmsResponseDto
+import com.tovelop.maphant.dto.SmsMessageReqDto
+import com.tovelop.maphant.dto.smsMessageConfirmReqDto
+import com.tovelop.maphant.service.RedisService
 import com.tovelop.maphant.service.SmsService
-import com.tovelop.maphant.type.response.SuccessResponse
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpSession
+import com.tovelop.maphant.type.response.Response
+import com.tovelop.maphant.type.response.ResponseUnit
+import com.tovelop.maphant.utils.ValidationHelper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.SessionAttribute
 
 @RestController
-class SmsController(private val smsService: SmsService) {
+class SmsController(@Autowired private val smsService: SmsService, @Autowired private val redisService: RedisService) {
     @PostMapping("/sms/send")
-    fun sendSms(@RequestBody messageDto: MessageDto, request: HttpServletRequest): SuccessResponse<SmsResponseDto> {
-        val result = smsService.sendSms(messageDto) as SmsResponseDto
+    fun sendSms(
+        @RequestBody smsMessageReqDto: SmsMessageReqDto,
+    ): ResponseEntity<ResponseUnit> {
+        if (ValidationHelper.isValidPhoneNum(smsMessageReqDto.to).not()) return ResponseEntity.badRequest()
+            .body(Response.error("올바른 전화번호가 아닙니다."))
 
-        //세션에 인증코드 저장
-        val session = request.getSession(true)
-        session.setAttribute("sms_code", result.smsConfirmNum)
-
-        return SuccessResponse(result)
+        return ResponseEntity.ok().body(Response.stateOnly(smsService.sendVerification(smsMessageReqDto.to)))
     }
 
     @GetMapping("/sms/check")
     fun codeCheck(
-        @RequestParam("code") sendCode: String,
-        @SessionAttribute(name = "sms_code") code: String
-    ): SuccessResponse<String> {
-        if (!sendCode.equals(code))
-            throw IllegalArgumentException("인증코드가 맞지 않습니다.")
+        @RequestBody smsMessageConfirmReqDto: smsMessageConfirmReqDto
+    ): ResponseEntity<ResponseUnit> {
+        val result = smsService.checkVerification(smsMessageConfirmReqDto.to, smsMessageConfirmReqDto.code)
 
-        return SuccessResponse("인증에 성공하였습니다.")
+        if (!result) return ResponseEntity.badRequest().body(Response.error("인증번호가 일치하지 않습니다."))
+
+        return ResponseEntity.ok().body(Response.stateOnly(result))
     }
 }
