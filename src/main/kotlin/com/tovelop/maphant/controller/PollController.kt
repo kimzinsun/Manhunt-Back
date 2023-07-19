d42270e41eb9797b26d378b85f3d74522e3a17f7
uomaep <uomaep@gmail.com>
2023-07-19 15:24:22 +0900

    feat: add poll post api

--- src/main/kotlin/com/tovelop/maphant/controller/PollController.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/PollController.kt
@@ -1,7 +1,19 @@
 package com.tovelop.maphant.controller
 
-import org.springframework.stereotype.Controller
+import com.tovelop.maphant.type.response.Response
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
+@RequestMapping("/poll")
 class PollController {
+    @PostMapping("/") // 투표 생성
+    fun createPoll(@RequestBody poll: PollDTO): ResponseEntity<Any> {
+        println(poll)
+
+        return ResponseEntity.ok().body(Response.stateOnly(true))
+    }
 }
