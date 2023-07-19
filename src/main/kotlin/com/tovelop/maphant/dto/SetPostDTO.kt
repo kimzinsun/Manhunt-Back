eba954b8d61903882d3fa9daa5ec09f4a165ce7c
pwrwpw <dlgustjr9912@gmail.com>
2023-07-19 20:23:40 +0900

    SetPostDTO 수정

--- src/main/kotlin/com/tovelop/maphant/dto/SetPostDTO.kt
+++ src/main/kotlin/com/tovelop/maphant/dto/SetPostDTO.kt
@@ -1,5 +1,7 @@
 package com.tovelop.maphant.dto
 
+import java.time.LocalDateTime
+
 data class SetPostDTO(
     val parentId: Int?,
     val categoryId: Int,
@@ -7,7 +9,28 @@ data class SetPostDTO(
     val typeId: Int,
     val title: String,
     val body: String,
-    val isHide: Byte,
-    val isComplete: Byte,
-    val isAnonymous: Byte,
-    )
+    val isHide: Boolean,
+    val isComplete: Boolean,
+    val isAnonymous: Boolean
+) {
+    fun toBoardDTO(): BoardDTO {
+        return BoardDTO(
+            parentId = parentId,
+            categoryId = categoryId,
+            userId = userId,
+            typeId = typeId,
+            title = title,
+            body = body,
+            state = 0,
+            isHide = isHide,
+            isComplete = isComplete,
+            isAnonymous = isAnonymous,
+            createAt = LocalDateTime.now(),
+            modifiedAt = LocalDateTime.now(),
+            commentCnt = 0,
+            likeCnt = 0,
+            reportCnt = 0,
+            imagesUrl = null
+        )
+    }
+}
