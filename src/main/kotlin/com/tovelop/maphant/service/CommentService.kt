36a682945d328d1e40049eaab4311c7787f0862b
wonindev <sangwonparkdev@gmail.com>
2023-07-20 14:29:50 +0900

    CommentService 작성

--- src/main/kotlin/com/tovelop/maphant/service/CommentService.kt
+++ src/main/kotlin/com/tovelop/maphant/service/CommentService.kt
@@ -1,8 +1,34 @@
 package com.tovelop.maphant.service
 
+import com.tovelop.maphant.dto.CommentDTO
 import com.tovelop.maphant.mapper.CommentMapper
 import org.springframework.stereotype.Service
 
 @Service
-class CommentService(val mapper: CommentMapper) {
+class CommentService(val commentMapper: CommentMapper) {
+    fun findAllComment(boardId: Int) = commentMapper.findAllComment(boardId)
+
+    fun insertComment(commentDTO: CommentDTO) = commentMapper.insertComment(commentDTO)
+
+
+    fun deleteComment(boardId: Int) = commentMapper.deleteComment(boardId)
+
+    fun updateComment(commentDTO: CommentDTO) = commentMapper.updateComment(commentDTO)
+
+    fun cntComment(boardId: Int) = commentMapper.cntComment(boardId)
+
+    fun insertCommentLike(userId: Int, commentId: Int) = commentMapper.insertCommentLike(userId, commentId)
+
+    fun findCommentLike(userId: Int, commentId: Int) = commentMapper.findCommentLike(userId, commentId)
+
+    fun cntCommentLike(commentId: Int) = commentMapper.cntCommentLike(commentId)
+
+    fun deleteCommentLike(userId: Int, commentId: Int) = commentMapper.deleteCommentLike(userId, commentId)
+
+    fun insertCommentReport(userId: Int, commentId: Int, reportReason: String) =
+        commentMapper.insertCommentReport(userId, commentId, reportReason)
+
+    fun findCommentReport(userId: Int, commentId: Int) = commentMapper.findCommentReport(userId, commentId)
+
+    fun cntCommentReport(commentId: Int) = commentMapper.cntCommentReport(commentId)
 }
