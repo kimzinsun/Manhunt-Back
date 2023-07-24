d8b62b9dfb2e79bca07385f094492d63c4464ed6
LeeJaeHoon <dlwogns3413@naver.com>
2023-07-24 16:18:38 +0900

    Feat: Profile bug fix

--- src/main/kotlin/com/tovelop/maphant/controller/ProfileController.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/ProfileController.kt
@@ -3,20 +3,18 @@ package com.tovelop.maphant.controller
 import com.tovelop.maphant.configure.security.token.TokenAuthToken
 import com.tovelop.maphant.dto.BoardDTO
 import com.tovelop.maphant.dto.ProfileImageDto
-import com.tovelop.maphant.dto.UploadLogDTO
 import com.tovelop.maphant.service.AwsS3Service
 import com.tovelop.maphant.service.ProfileService
-import com.tovelop.maphant.service.UploadLogService
 import com.tovelop.maphant.type.response.SuccessResponse
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
-@Controller
+@RestController
 @RequestMapping("/profile")
 class ProfileController (
     val profileService: ProfileService,
@@ -27,7 +25,6 @@ class ProfileController (
     fun getProfileImage(): SuccessResponse<ProfileImageDto>{
         val auth = SecurityContextHolder.getContext().authentication!! as TokenAuthToken
         val userId:Int = auth.getUserData().id!!
-
         return SuccessResponse(profileService.getProfileImage(userId))
     }
 
@@ -37,16 +34,16 @@ class ProfileController (
         val userId: Int = auth.getUserData().id!!
 
         val imageUrl:String = awsS3Service.uploadFile(file)
+        // TODO: userId에 해당하는 profile 튜플이 없는 경우 insert 있는 경우 update
         profileService.updateProfileImage(userId, imageUrl, file)
 
         return SuccessResponse(imageUrl)
     }
 
     @GetMapping("/board")
-    fun getPostsList(userInt: Int):SuccessResponse<List<BoardDTO>>{
+    fun getPostsList():SuccessResponse<List<BoardDTO>>{
         val auth = SecurityContextHolder.getContext().authentication!! as TokenAuthToken
         val userId:Int = auth.getUserData().id!!
-
         return SuccessResponse(profileService.getBoardsList(userId))
     }
 }
--- src/main/kotlin/com/tovelop/maphant/dto/ProfileImageDto.kt
+++ src/main/kotlin/com/tovelop/maphant/dto/ProfileImageDto.kt
@@ -3,6 +3,6 @@ package com.tovelop.maphant.dto
 import java.time.LocalDate
 
 data class ProfileImageDto(
-    val user_name : String,
-    val profile_img : String,
+    val user_nickname : String,
+    val profile_img : String?,
 )
