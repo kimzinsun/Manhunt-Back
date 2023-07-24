ec97e59c4e0de3036493c90315de42c04a7a0e89
kou466 <1226hajing@gmail.com>
2023-07-24 15:57:48 +0900

    comment comtroller, mapper 수정

--- src/main/kotlin/com/tovelop/maphant/controller/CommentController.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/CommentController.kt
@@ -14,14 +14,14 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/comment")
 class CommentController(@Autowired val commentService: CommentService) {
 
-    @GetMapping("/list")
-    fun findAllComment(boardId: Int): ResponseEntity<Response<List<CommentDTO>>> {
+    @GetMapping("/list/{boardId}")
+    fun findAllComment(@PathVariable boardId: Int): ResponseEntity<Response<List<CommentDTO>>> {
         val comment = commentService.findAllComment(boardId)
         return ResponseEntity.ok().body(Response.success(comment))
     }
 
     @PostMapping("/insert")
-    fun insertComment(commentDTO: CommentDTO): ResponseEntity<ResponseUnit> {
+    fun insertComment(@RequestBody commentDTO: CommentDTO): ResponseEntity<ResponseUnit> {
         if (commentDTO.body.isBlank()) {
             return ResponseEntity.badRequest().body(Response.error("댓글 내용을 입력해주세요."))
         }
@@ -29,33 +29,35 @@ class CommentController(@Autowired val commentService: CommentService) {
         return ResponseEntity.ok().body(Response.stateOnly(true))
     }
 
-    @PostMapping("/delete")
-    fun deleteComment(userId: Int, commentId: Int): ResponseEntity<ResponseUnit> {
-        val comment = commentService.getCommentById(commentId)
-        if (comment.user_id != userId) {
-            return ResponseEntity.badRequest().body(Response.error("자신의 댓글만 삭제할 수 있습니다."))
-        }
+    @DeleteMapping("/delete/{userId}/{commentId}")
+    fun deleteComment(@PathVariable userId: Int, @PathVariable commentId: Int): ResponseEntity<ResponseUnit> {
+//        val comment = commentService.getCommentById(commentId)
+//        if (comment.user_id != userId) {
+//            return ResponseEntity.badRequest().body(Response.error("자신의 댓글만 삭제할 수 있습니다."))
+//        }
+        // TODO : 자신의 댓글만 삭제 할 수 있도록 수정하기
         commentService.deleteComment(userId, commentId)
         return ResponseEntity.ok().body(Response.stateOnly(true))
     }
 
     @PostMapping("/update")
-    fun updateComment(@RequestBody commentDTO: CommentDTO, @RequestParam userId: Int): ResponseEntity<ResponseUnit> {
-        val comment = commentService.getCommentById(commentDTO.id)
-        if (comment.user_id != userId) {
-            return ResponseEntity.badRequest().body(Response.error("자신의 댓글만 수정할 수 있습니다."))
-        }
+    fun updateComment(@RequestBody commentDTO: CommentDTO): ResponseEntity<ResponseUnit> {
+//        val comment = commentService.getCommentById(commentDTO.id)
+//        if (comment.user_id != userId) {
+//            return ResponseEntity.badRequest().body(Response.error("자신의 댓글만 수정할 수 있습니다."))
+//        }
         commentService.updateComment(commentDTO)
         return ResponseEntity.ok().body(Response.stateOnly(true))
     }
 
     @PostMapping("/insert-like")
     fun insertCommentLike(userId: Int, commentId: Int): ResponseEntity<ResponseUnit> {
-        if (commentService.findCommentLike(userId, commentId) != 0) {
-            return ResponseEntity.badRequest().body(Response.error("이미 좋아요를 누른 댓글입니다."))
-        }
+//        if (commentService.findCommentLike(userId, commentId) != 0) {
+//            return ResponseEntity.badRequest().body(Response.error("이미 좋아요를 누른 댓글입니다."))
+//        }
         commentService.insertCommentLike(userId, commentId)
         return ResponseEntity.ok().body(Response.stateOnly(true))
+        // TODO : 여기부터 수정하기
     }
 
     @PostMapping("/find-like")
@@ -72,10 +74,10 @@ class CommentController(@Autowired val commentService: CommentService) {
 
     @PostMapping("/delete-like")
     fun deleteCommentLike(userId: Int, commentId: Int): ResponseEntity<ResponseUnit> {
-        val comment = commentService.getCommentById(commentId)
-        if (comment.user_id != userId) {
-            return ResponseEntity.badRequest().body(Response.error("자신의 댓글만 취소할 수 있습니다."))
-        }
+//        val comment = commentService.getCommentById(commentId)
+//        if (comment.user_id != userId) {
+//            return ResponseEntity.badRequest().body(Response.error("자신의 댓글만 취소할 수 있습니다."))
+//        }
         commentService.deleteCommentLike(userId, commentId)
         return ResponseEntity.ok().body(Response.stateOnly(true))
     }
