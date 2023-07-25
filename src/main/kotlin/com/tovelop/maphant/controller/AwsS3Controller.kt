b5e963f994d65e5977771a19d92ee1f5f9f173b4
uomaep <uomaep@gmail.com>
2023-07-25 16:56:52 +0900

    .

--- src/main/kotlin/com/tovelop/maphant/controller/AwsS3Controller.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/AwsS3Controller.kt
@@ -24,12 +24,8 @@ class AwsS3Controller(
             // 에러 던지기
             throw MaxUploadSizeExceededException(5)
         }
-        // 시큐리티 붙이기전 userId 1로 가정
-        // 아직 Security Configure가 끝나지 않아서 익명 유저 토큰도 들어옴.
-        // HTTP 헤더에 x-mockup-auth가 있어야 MockupCustomUserToken으로 캐스팅 가능
-        // 캐스팅이 안되면 익명 유저 토큰으로 캐스팅 (오류 발생함 - AnonymousAuthenticationToken cannot be cast to MockupCustomUserToken)
-        val securityContextHolder = SecurityContextHolder.getContext().authentication as TokenAuthToken
-        val userId: Int = securityContextHolder.getUserData().id
+        val auth = SecurityContextHolder.getContext().authentication!! as TokenAuthToken
+        val userId: Int = auth.getUserData().id!!
 
         // 서비스에서 여러 파일 aws에 올려서 url 받기
         val imageUrls: List<String> = awsS3Service.uploadFiles(files)
