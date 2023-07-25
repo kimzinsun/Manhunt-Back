14926c117328f9d0797fa90fd55793164cac6a99
uomaep <uomaep@gmail.com>
2023-07-25 16:41:27 +0900

    fix: Mockup class 삭제 및 토큰 저장 방식에 대한 뒷처리

--- src/main/kotlin/com/tovelop/maphant/controller/MockupSignInController.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/MockupSignInController.kt
@@ -1,19 +1,11 @@
 package com.tovelop.maphant.controller
 
-import com.tovelop.maphant.configure.MockupCustomUser
-import com.tovelop.maphant.configure.MockupCustomUserToken
-import com.tovelop.maphant.configure.security.UserData
 import com.tovelop.maphant.configure.security.token.TokenAuthToken
-import com.tovelop.maphant.dto.MockupUserDTO
 import com.tovelop.maphant.dto.UserDataDTO
 import com.tovelop.maphant.type.response.Response
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 data class SignInDto(val email: String, val password: String)
 
@@ -21,10 +13,10 @@ data class SignInDto(val email: String, val password: String)
 @RestController
 class TestSignInController {
     @PostMapping("/signIn")
-    fun signIn(@RequestBody signInDto: SignInDto): ResponseEntity<Response<MockupCustomUser>> {
+    fun signIn(@RequestBody signInDto: SignInDto): ResponseEntity<Response<UserDataDTO>> {
         val auth = SecurityContextHolder.getContext().authentication
-        if (auth != null && auth is MockupCustomUserToken && auth.isAuthenticated) {
-            return ResponseEntity.ok(Response.success(auth.details))
+        if (auth != null && auth is TokenAuthToken && auth.isAuthenticated) {
+            return ResponseEntity.ok(Response.success(auth.getUserData()))
         }
 
         return ResponseEntity.unprocessableEntity().body(Response.error("Invalid credentials"))
