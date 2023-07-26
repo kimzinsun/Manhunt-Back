88c1a8ada06ce135f1552e88f10c5600b20de560
wjswlghks77 <wjswlghks77@naver.com>
2023-07-26 18:17:25 +0900

    first commit

--- src/main/kotlin/com/tovelop/maphant/mapper/AdminPageMapper.kt
+++ src/main/kotlin/com/tovelop/maphant/mapper/AdminPageMapper.kt
@@ -1,4 +1,12 @@
 package com.tovelop.maphant.mapper
 
+import com.tovelop.maphant.dto.UserDTO
+
 interface AdminPageMapper {
+    fun isAdmin(userDTO: UserDTO): Boolean
+    fun setUserRole(role: String, id: Int)
+    fun findBoardReport()
+    fun setBoardReport(boardId: Int)
+    fun findCommentReport()
+    fun setCommentReport(commentId: Int)
 }
--- src/main/kotlin/com/tovelop/maphant/service/AdminPageService.kt
+++ src/main/kotlin/com/tovelop/maphant/service/AdminPageService.kt
@@ -1,4 +1,33 @@
 package com.tovelop.maphant.service
 
-class AdminPageService {
+import com.tovelop.maphant.dto.UserDTO
+import com.tovelop.maphant.mapper.AdminPageMapper
+import com.tovelop.maphant.type.response.Response
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+
+class AdminPageService(@Autowired val adminPageMapper: AdminPageMapper) {
+    fun isAdmin(userDTO: UserDTO): Boolean {
+        if(userDTO.role == "admin") return true
+        else return false
+//        if (auth.getUserData().role != "admin" || auth.getUserData().id != boardService.getUserIdByBoardId(boardId)) {
+//            return ResponseEntity.badRequest().body(Response.error<Any>("권한이 없습니다."))
+//        }
+    }
+    fun setUserRole(role: String, id: Int) {
+        adminPageMapper.setUserRole(role, id)
+    }
+    fun findBoardReport() {
+        adminPageMapper.findBoardReport()
+    }
+    fun setBoardReport(boardId: Int) {
+        adminPageMapper.setBoardReport(boardId)
+    }
+    fun findCommentReport() {
+        adminPageMapper.findBoardReport()
+    }
+    fun setCommentReport(commentId: Int) {
+        adminPageMapper.setBoardReport(commentId)
+    }
+
 }
