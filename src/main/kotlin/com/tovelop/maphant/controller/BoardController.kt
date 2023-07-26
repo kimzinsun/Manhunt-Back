b714e178580fd645290c5c0117ec369688024252
pwrwpw <dlgustjr9912@gmail.com>
2023-07-26 18:33:57 +0900

    boardController 수정

--- src/main/kotlin/com/tovelop/maphant/controller/BoardController.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/BoardController.kt
@@ -152,22 +152,12 @@ class BoardController(@Autowired val boardService: BoardService) {
     fun searchBoard(@RequestParam content:String): Any {
         val searchBoard = boardService.findBoardByKeyword(content)
         // 검색어가 포함된 게시글 읽어오기
-        print("content: $content")
-        print("searchBoard: $searchBoard")
         if(searchBoard.isEmpty()){
             return ResponseEntity.badRequest().body(Response.error<Unit>("검색 결과가 없습니다."))
         }
         // return: json
         return ResponseEntity.ok(Response.success(searchBoard))
     }
-    @GetMapping("/my")
-    fun readMyBoard(@RequestBody board: SetBoardDTO): ResponseEntity<ResponseUnit> {
-        // 내가 쓴 게시글 읽어오기
-
-        // return: json
-        return ResponseEntity.ok(Response.stateOnly(true))
-    }
-
     @PostMapping("/report")
     fun reportBoard(@RequestBody board: SetBoardDTO): ResponseEntity<ResponseUnit> {
         // 신고하기
