c737511a16a2c18f6fc6aa8b77bce6932da68da8
CodingApe9 <pos06098@gmail.com>
2023-07-30 14:55:46 +0900

    refac findBoardReport / add findReportInfo

--- src/main/kotlin/com/tovelop/maphant/mapper/AdminPageMapper.kt
+++ src/main/kotlin/com/tovelop/maphant/mapper/AdminPageMapper.kt
@@ -1,6 +1,7 @@
 package com.tovelop.maphant.mapper
 
 import com.tovelop.maphant.dto.AdminBoardReportDTO
+import com.tovelop.maphant.dto.BoardReportInfoDTO
 import com.tovelop.maphant.dto.UserDTO
 import org.apache.ibatis.annotations.Mapper
 import org.springframework.stereotype.Repository
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Repository
 interface AdminPageMapper {
     fun findBoardReportByReportedAt(reportSize: Int): List<AdminBoardReportDTO>
     fun findBoardReportByMostReportedRanking(reportSize: Int): List<AdminBoardReportDTO>
+    fun findBoardReportBySortType(reportSize: Int, sortType: String): List<AdminBoardReportDTO>
+    fun findReportInfo(boardId: Int): List<BoardReportInfoDTO>
 
 //    fun setBoardSanction(boardId: Int)
 //    fun findCommentReport()
--- src/main/kotlin/com/tovelop/maphant/service/AdminPageService.kt
+++ src/main/kotlin/com/tovelop/maphant/service/AdminPageService.kt
@@ -1,6 +1,7 @@
 package com.tovelop.maphant.service
 
 import com.tovelop.maphant.dto.AdminBoardReportDTO
+import com.tovelop.maphant.dto.BoardReportInfoDTO
 import com.tovelop.maphant.mapper.AdminPageMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -15,6 +16,8 @@ class AdminPageService(@Autowired val adminPageMapper: AdminPageMapper, @Autowir
     }
 
     /**
+     * 신고 당한 글에 대한 정보들만 가져온다.
+     * 신고 내용과 신과자의 대한 정보는 findReportInfo()를 통해 따로 응답.
      * sortType: 정렬 기준 (reportedAt: 오래된 신고부터, mostReportedRanking: 신고를 많이 받은 순서로)
      * reportSize: 불러올 신고의 개수.
      */
@@ -22,8 +25,11 @@ class AdminPageService(@Autowired val adminPageMapper: AdminPageMapper, @Autowir
         return when(sortType){
             "reportedAt" -> adminPageMapper.findBoardReportByReportedAt(reportSize)
             "mostReportedRanking" -> adminPageMapper.findBoardReportByMostReportedRanking(reportSize)
-            else -> adminPageMapper.findBoardReportByMostReportedRanking(reportSize)
+            else -> adminPageMapper.findBoardReportBySortType(reportSize, sortType)
         }
+    }
+    fun findReportInfo(boardId: Int): List<BoardReportInfoDTO>{
+        return adminPageMapper.findReportInfo(boardId)
     }
 //    fun insertBoardSanction(boardId: Int) {
 //        adminPageMapper.setBoardSanction(boardId)
