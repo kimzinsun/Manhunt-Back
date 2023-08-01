c116b98bd8a11944af8e67eee584c70fa1f49c3b
uomaep <uomaep@gmail.com>
2023-08-01 16:26:45 +0900

    fix: token: String -> List<String>

--- src/main/kotlin/com/tovelop/maphant/dto/FcmMessageDTO.kt
+++ src/main/kotlin/com/tovelop/maphant/dto/FcmMessageDTO.kt
@@ -4,7 +4,7 @@ import java.time.LocalDateTime
 
 data class FcmMessageDTO(
     val userId: Int,
-    val to: String, // receiver's-fcm-token
+    val to: List<String>? = null, // receiver's-fcm-token
     val title: String,
     val body: String,
     val etc: Map<String, String>, // send more key-value (ex. chatting service - sender, message, etc...)*/
