8e41ecee16564cd0b1cdb718e745a8f813858307
pwrwpw <dlgustjr9912@gmail.com>
2023-08-05 01:21:38 +0900

    Redis 수정 및 RateLimitingService파일 수정

--- src/main/kotlin/com/tovelop/maphant/service/RateLimitingService.kt
+++ src/main/kotlin/com/tovelop/maphant/service/RateLimitingService.kt
@@ -3,17 +3,33 @@ package com.tovelop.maphant.service
 import org.springframework.beans.factory.annotation.Autowired
 
 class RateLimitingService(@Autowired val redisService: RedisService) {
-    fun requestCheck(userId: String){
+    fun requestCheck(userId: String, requestType: String){
+        val incValue = when(requestType){
+            "WRITE_POST" -> 2
+            "WRITE_COMMENT" -> 1
+            else -> 0
+        }
         val key = "WRITE_REQUEST|$userId"
         val value = redisService.get(key)?.toIntOrNull() ?: 0 // 요청 횟수
-        redisService.set(key, (value+1).toString())
-        if (value == 1){ //첫 요청시 1분간 유효
-           // redisService.expire(key, 60)
+        redisService.set(key, (value+incValue).toString())
+        if (value == 1 && requestType == "WRITE_COMMENT"){ //댓글 첫 요청시 1분간 유효
+            redisService.expire(key, 60)
         }
-        // 1분간 요청이 5회 이상이면 밴
-        if (value >= 5){
-            //redisService.set("BAN|$userId", "true")
-            //redisService.expire("BAN|$userId", 60*60*24)
+        else if(value == 2 && requestType == "WRITE_POST"){ //글쓰기 첫 요청시 1분간 유효
+            redisService.expire(key, 60)
         }
+
+        // 1분간 요청이 댓글 , 글 포함해서 10번 이상이면 1일간 차단
+        if (value >= 10){
+            val banKey = "BAN|$userId"
+            if (redisService.get(banKey) == null){
+                redisService.set(banKey, "true")
+                redisService.expire(banKey, 60*60*24)
+            }
+        }
+    }
+    fun isBanned(userId: String): Boolean{
+        val banKey = "BAN|$userId"
+        return redisService.get(banKey) != null
     }
 }
