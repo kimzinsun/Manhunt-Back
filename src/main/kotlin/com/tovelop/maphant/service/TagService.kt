b629d5fc894c2103c183fad81eac3c4c1b028907
wonindev <sangwonparkdev@gmail.com>
2023-08-07 14:34:43 +0900

    TagService 작성

---
+++ src/main/kotlin/com/tovelop/maphant/service/TagService.kt
@@ -0,0 +1,29 @@
+package com.tovelop.maphant.service
+
+class TagService {
+
+}
+
+fun getTagList() {
+
+}
+
+fun insertTag() {
+
+}
+
+fun getTagByName() {
+
+}
+
+fun updateTag() {
+
+}
+
+fun updateTagCnt() {
+
+}
+
+fun deleteTagCnt() {
+
+}
