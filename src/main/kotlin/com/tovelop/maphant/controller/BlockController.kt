24213a5d3f44aaf737b399249e179dfa17e5466b
LeeJaeHoon <dlwogns3413@naver.com>
2023-08-07 14:23:48 +0900

    Feat - BlockController 추가

---
+++ src/main/kotlin/com/tovelop/maphant/controller/BlockController.kt
@@ -0,0 +1,10 @@
+package com.tovelop.maphant.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/block")
+class BlockController {
+
+}
