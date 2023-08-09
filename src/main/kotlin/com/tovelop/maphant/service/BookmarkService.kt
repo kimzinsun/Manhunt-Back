9031979b688a6728b74898e075db3902b7752954
cottonrose1011 <pjeka7er43@gmail.com>
2023-08-09 14:39:08 +0900

    Feat) add Delete

--- src/main/kotlin/com/tovelop/maphant/service/BookmarkService.kt
+++ src/main/kotlin/com/tovelop/maphant/service/BookmarkService.kt
@@ -2,16 +2,17 @@ package com.tovelop.maphant.service
 
 import com.tovelop.maphant.dto.BookmarkDTO
 import com.tovelop.maphant.mapper.BookmarkMapper
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class BookmarkService(val bookmarkMapper: BookmarkMapper) {
+class BookmarkService(@Autowired val bookmarkMapper: BookmarkMapper) {
 
     fun insert(userId: Int, boardId: Int): Boolean {
         try {
             bookmarkMapper.insertBoard(userId, boardId)
         } catch (e: Exception) {
-            //이미 추가됐거나, 북마크 전에 삭제된 경우
+            //이미 추가 됐거나, 북마크 전에 삭제된 경우
             return false
         }
         return true
@@ -19,5 +20,15 @@ class BookmarkService(val bookmarkMapper: BookmarkMapper) {
 
     fun showBookmarks(userId: Int): Result<List<BookmarkDTO>> {
         return Result.runCatching { bookmarkMapper.selectBoardAllById(userId) }
+    }
+
+    fun deleteBookmark(userId: Int, boardId: Int): Boolean {
+        try {
+            bookmarkMapper.deleteBoardById(userId, boardId)
+        } catch (e: Exception) {
+            // 삭제 실패
+            return false
+        }
+        return true
     }
 }
