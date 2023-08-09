5547639da81e62ca2f30c3edefada96f17bdd075
cottonrose1011 <pjeka7er43@gmail.com>
2023-08-09 14:39:08 +0900

    Feat) add Delete

--- src/main/kotlin/com/tovelop/maphant/controller/BookmarkController.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/BookmarkController.kt
@@ -3,24 +3,22 @@ package com.tovelop.maphant.controller
 import com.tovelop.maphant.configure.security.token.TokenAuthToken
 import com.tovelop.maphant.service.BookmarkService
 import com.tovelop.maphant.type.response.Response
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 
 @Controller
 @RequestMapping("/bookmark")
-class BookmarkController(val bookmarkService: BookmarkService) {
+class BookmarkController(@Autowired val bookmarkService: BookmarkService) {
 
     //북마크 추가 api
     @PostMapping("/{boardId}")
     fun bookmark(@PathVariable("boardId") boardId: Int): ResponseEntity<Any> {
         val auth = SecurityContextHolder.getContext().authentication!! as TokenAuthToken
 
-        val bookmarkResult = bookmarkService.insert(auth.getUserId()!!, boardId)
+        val bookmarkResult = bookmarkService.insert(auth.getUserId(), boardId)
         if (!bookmarkResult) {
             return ResponseEntity.badRequest().body(
                 Response.error<Any>
@@ -38,11 +36,22 @@ class BookmarkController(val bookmarkService: BookmarkService) {
             return ResponseEntity.badRequest().body("로그인 안됨")
         }
 
-        val bookmarkList = bookmarkService.showBookmarks(auth.getUserId()!!)
+        val bookmarkList = bookmarkService.showBookmarks(auth.getUserId())
         if (bookmarkList.isFailure) {
             return ResponseEntity.badRequest().body(Response.error<Any>("요청에 실패했습니다."))
         }
 
         return ResponseEntity.ok().body(Response.success(bookmarkList.getOrNull()))
+    }
+
+    @DeleteMapping("/{boardId}")
+    fun deleteBookmarks(@PathVariable("boardId") boardId: Int): ResponseEntity<Any> {
+        val auth = SecurityContextHolder.getContext().authentication as TokenAuthToken
+        val deleteResult = bookmarkService.deleteBookmark(auth.getUserId(), boardId)
+        if (!deleteResult) {
+            return ResponseEntity.badRequest().body(Response.error<Any>("북마크 삭제를 실패했습니다."))
+        }
+
+        return ResponseEntity.ok().body(Response.stateOnly(true))
     }
 }
