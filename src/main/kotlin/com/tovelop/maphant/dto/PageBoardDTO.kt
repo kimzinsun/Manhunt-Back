640fa8a407f9602282d92679a012373a0a5bb5e2
LeeJaeHoon <dlwogns3413@naver.com>
2023-08-09 22:06:11 +0900

    Feat: Hot게시글 전체 조회, 게시판 마다 HOT게시글 조회시 tag 추가

--- src/main/kotlin/com/tovelop/maphant/dto/PageBoardDTO.kt
+++ src/main/kotlin/com/tovelop/maphant/dto/PageBoardDTO.kt
@@ -5,6 +5,7 @@ import java.time.LocalDateTime
 data class PageBoardDTO(
     val boardId : Int,
     val title: String,
+    val body: String,
     val createdAt: LocalDateTime,
     val modifiedAt : LocalDateTime?,
     val userNickname: String,
