22755e541f25a90ff861e58fedf04f36434ca2e6
LeeJaeHoon <dlwogns3413@naver.com>
2023-08-09 13:58:49 +0900

    Fix: SendDm  DeadLock 걸리는 상황 해결

--- src/main/kotlin/com/tovelop/maphant/mapper/RoomMapper.kt
+++ src/main/kotlin/com/tovelop/maphant/mapper/RoomMapper.kt
@@ -10,7 +10,7 @@ import java.time.LocalDateTime
 @Mapper
 @Repository
 interface RoomMapper {
-    fun findRoom(sender_id: Int, receiver_id: Int): RoomDto
+    fun findRoom(sender_id: Int, receiver_id: Int): RoomDto?
     fun createRoom(createHeaderDto: CreateRoomDto): Boolean
     fun updateSenderUnreadCountAndLastContent(room_id: Int, content: String): Boolean
     fun updateReceiverUnreadCountAndLastContent(room_id: Int, content: String): Boolean
@@ -28,4 +28,6 @@ interface RoomMapper {
 
     fun updateSenderDmCursor(room_id: Int): Boolean
     fun updateReceiverDmCursor(room_id: Int):Boolean
+
+    fun updateRoomWhenSendDm(time:LocalDateTime, content: String, isSender:Boolean, room_id: Int):Boolean
 }
