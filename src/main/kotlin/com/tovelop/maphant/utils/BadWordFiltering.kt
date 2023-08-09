e8911ca285299d745d696642493f606387cf471e
wonindev <sangwonparkdev@gmail.com>
2023-08-09 13:58:45 +0900

    비속어필터 함수 생성

--- src/main/kotlin/com/tovelop/maphant/utils/BadWordFiltering.kt
+++ src/main/kotlin/com/tovelop/maphant/utils/BadWordFiltering.kt
@@ -1,5 +1,41 @@
 package com.tovelop.maphant.utils
 
 class BadWordFiltering {
-    
+    var badWords = listOf<String>()
+
+    fun filter(text: String): String {
+        var result = text
+        badWords.forEach { badWord ->
+            result = result.replace(badWord, "*".repeat(badWord.length))
+        }
+        return result
+    }
+
+    fun addBadWord(badWord: String) {
+        badWords += badWord
+    }
+
+    fun removeBadWord(badWord: String) {
+        badWords -= badWord
+    }
+
+    fun clearBadWords() {
+        badWords = listOf()
+    }
+
+    fun getBadWords(): List<String> {
+        return badWords
+    }
+
+    fun setBadWords(badWords: List<String>) {
+        this.badWords = badWords
+    }
+
+    fun getBadWordsCount(): Int {
+        return badWords.size
+    }
+
+    fun isBadWord(text: String): Boolean {
+        return badWords.contains(text)
+    }
 }
