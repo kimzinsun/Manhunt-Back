0a5cc47fd7682ffe6a5d26034d0483c35f61fec4
Choijake <shameless8@naver.com>
2023-08-10 13:37:59 +0900

    feat: 태그 수정 삭제 서비스 작성

--- src/main/kotlin/com/tovelop/maphant/mapper/TagMapper.kt
+++ src/main/kotlin/com/tovelop/maphant/mapper/TagMapper.kt
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Repository
 @Repository
 interface TagMapper {
     fun insertTag(categoryId: Int, boardId: Int, tagName: String)
+    fun deleteTag(boardId: Int, tagId: Int):Boolean
+
+    //게시글 수정 시 해당 함수의 반환값이 0일때만 태그 추가 가능
+    fun getTagExistenceInBoard(boardId: Int, tagId: Int):Int
     fun insertBoardTag(boardId: Int, tagId: Int)
     fun getTagByName(name: String): TagDTO?
     fun getTagList(): List<TagDTO>
--- src/main/kotlin/com/tovelop/maphant/service/TagService.kt
+++ src/main/kotlin/com/tovelop/maphant/service/TagService.kt
@@ -11,6 +11,14 @@ class TagService(private val tagMapper: TagMapper) {
         }
     }
 
+    fun deleteTag(boardId: Int, tagId: Int):Boolean = tagMapper.deleteTag(boardId, tagId)
+
+    fun modifyTag(categoryId: Int, boardId: Int, tagId: Int, tagName: String){
+        if(tagMapper.getTagExistenceInBoard(boardId, tagId)==0){
+            tagMapper.insertTag(categoryId, boardId, tagName.replace("[^\\p{L}\\p{N}_]".toRegex(), ""))
+        }
+    }
+
     fun insertBoardTag(boardId: Int, tagId: Int) = tagMapper.insertBoardTag(boardId, tagId)
 
     fun getTagByName(name: String) = tagMapper.getTagByName(name.replace("[^\\p{L}\\p{N}_]".toRegex(), ""))
