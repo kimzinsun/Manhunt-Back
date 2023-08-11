53e5ecce9d2a571281dc24cb7cf4e326e9c18c54
HanDaWoon <bookpia.han@gmail.com>
2023-08-11 16:39:41 +0900

    Feat : 프로필 변경 시 기본 이미지로 변경 가능

--- src/main/kotlin/com/tovelop/maphant/controller/ProfileController.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/ProfileController.kt
@@ -1,22 +1,18 @@
 package com.tovelop.maphant.controller
 
 import com.tovelop.maphant.configure.security.token.TokenAuthToken
-import com.tovelop.maphant.dto.*
+import com.tovelop.maphant.dto.BoardResDto
+import com.tovelop.maphant.dto.CommentExtDTO
+import com.tovelop.maphant.dto.ProfileNicknameAndBodyAndImageDto
 import com.tovelop.maphant.service.AwsS3Service
 import com.tovelop.maphant.service.ProfileService
 import com.tovelop.maphant.type.paging.PagingDto
 import com.tovelop.maphant.type.paging.PagingResponse
 import com.tovelop.maphant.type.response.Response
-import com.tovelop.maphant.type.response.SuccessResponse
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
@@ -30,7 +26,7 @@ class ProfileController(
     fun getProfile(@RequestParam targetUserId: Int?): ResponseEntity<Response<ProfileNicknameAndBodyAndImageDto>> {
         val userId: Int = (SecurityContextHolder.getContext().authentication as TokenAuthToken).getUserId()
         return ResponseEntity.ok()
-            .body(Response.success(profileService.getNicknameAndBodyAndImage(targetUserId ?: userId)));
+            .body(Response.success(profileService.getNicknameAndBodyAndImage(targetUserId ?: userId)))
     }
 
     @PatchMapping
@@ -45,12 +41,13 @@ class ProfileController(
         if (nickname != null) profileService.updateProfileNickname(userId, nickname)
         if (body != null) profileService.updateProfileBody(userId, body)
         if (file != null) {
-            val imageUrl: String = awsS3Service.uploadFile(file)
-            profileService.updateProfileImage(userId, imageUrl, file)
+            if (file.isEmpty) profileService.updateProfileImageDefault(userId) else {
+                val imageUrl: String = awsS3Service.uploadFile(file)
+                profileService.updateProfileImage(userId, imageUrl, file)
+            }
         }
 
-
-        return ResponseEntity.ok().body(Response.success("프로필 수정 성공"));
+        return ResponseEntity.ok().body(Response.success("프로필 수정 성공"))
     }
 
     @GetMapping("/comment")
@@ -60,7 +57,7 @@ class ProfileController(
     ): ResponseEntity<Response<PagingResponse<CommentExtDTO>>> {
         val userId: Int = (SecurityContextHolder.getContext().authentication as TokenAuthToken).getUserId()
         return ResponseEntity.ok()
-            .body(Response.success(profileService.getCommentList(userId, targetUserId ?: userId, pagingDto)));
+            .body(Response.success(profileService.getCommentList(userId, targetUserId ?: userId, pagingDto)))
     }
 
     @GetMapping("/board")
@@ -70,6 +67,6 @@ class ProfileController(
     ): ResponseEntity<Response<PagingResponse<BoardResDto>>> {
         val userId: Int = (SecurityContextHolder.getContext().authentication as TokenAuthToken).getUserId()
         return ResponseEntity.ok()
-            .body(Response.success(profileService.getBoardsList(userId, targetUserId ?: userId, pagingDto)));
+            .body(Response.success(profileService.getBoardsList(userId, targetUserId ?: userId, pagingDto)))
     }
 }
