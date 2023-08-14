e8b1e7c025e191b7fe32c9caff5b8a0e3bbc03dd
wjswlghks77 <wjswlghks77@naver.com>
2023-08-14 14:47:08 +0900

    refact

--- src/main/kotlin/com/tovelop/maphant/controller/AdminPageController.kt
+++ src/main/kotlin/com/tovelop/maphant/controller/AdminPageController.kt
@@ -28,40 +28,48 @@ import org.springframework.web.bind.annotation.RestController
 
 @Controller
 @RequestMapping("/admin")
-class AdminPageController(@Autowired val adminPageService: AdminPageService, @Autowired val bannerService: BannerService) {
+class AdminPageController(
+    @Autowired val adminPageService: AdminPageService,
+    @Autowired val bannerService: BannerService
+) {
     @GetMapping("/")
     fun listBoardReport(model: Model, @RequestParam sortType: String?): String {
-        val findBoardReport = adminPageService.findBoardReport(sortType?:"reportedAt", 5)
-        val findCommentReport = adminPageService.findCommentReport(sortType?:"reportedAt", 5)
+        val findBoardReport = adminPageService.findBoardReport(sortType ?: "reportedAt", 5)
+        val findCommentReport = adminPageService.findCommentReport(sortType ?: "reportedAt", 5)
         val findUserList = adminPageService.findAllUserSanction()
         model.addAttribute("boardReport", findBoardReport)
         model.addAttribute("commentReport", findCommentReport)
         model.addAttribute("userSanction", findUserList)
         return "admin_index_page"
     }
+
     @GetMapping("/reportInfo/board")
     fun boardReportInfo(@RequestParam boardId: Int): ResponseEntity<Response<List<BoardReportInfoDTO>>> {
         val findBoardReportInfo = adminPageService.findBoardReportInfo(boardId)
         return ResponseEntity.ok().body(Response.success(findBoardReportInfo))
     }
+
     @GetMapping("/reportInfo/comment")
     fun commentReportInfo(@RequestParam commentId: Int): ResponseEntity<Response<List<CommentReportInfoDTO>>> {
         val findBoardReportInfo = adminPageService.findCommentReportInfo(commentId)
         return ResponseEntity.ok().body(Response.success(findBoardReportInfo))
     }
+
     @PostMapping("/sanction/board")
     fun sanctionBoard(@RequestParam boardId: Int): ResponseEntity<ResponseUnit> {
         adminPageService.updateBoardSanction(boardId)
         return ResponseEntity.ok(Response.stateOnly(true))
     }
+
     @PostMapping("/sanction/comment")
     fun sanctionComment(@RequestParam commentId: Int): ResponseEntity<ResponseUnit> {
         adminPageService.updateCommentSanction(commentId)
         return ResponseEntity.ok(Response.stateOnly(true))
     }
+
     @PostMapping("/sanction/user")
     fun sanctionUser(@RequestBody userReportDTO: UserReportDTO): ResponseEntity<ResponseUnit> {
-        if(adminPageService.findReportByUserId(userReportDTO.userId)) {
+        if (adminPageService.findReportByUserId(userReportDTO.userId)) {
             return ResponseEntity.ok(Response.stateOnly(false))
         }
         //유저를 정지 상태(2)로 변경
@@ -73,6 +81,7 @@ class AdminPageController(@Autowired val adminPageService: AdminPageService, @Au
         adminPageService.updateUserState(userReportDTO.userId, 2)
         return ResponseEntity.ok(Response.stateOnly(true))
     }
+
     @PostMapping("/unSanction/user")
     fun unSanctionUser(@RequestParam userId: Int): ResponseEntity<ResponseUnit> {
         //유저 제재 내역 테이블에서 삭제
@@ -80,47 +89,60 @@ class AdminPageController(@Autowired val adminPageService: AdminPageService, @Au
         //유저를 정상 상태(1)로 변경
         adminPageService.updateUserState(userId, 1)
     }
+
     @GetMapping("/reportInfo/user")
     fun userReportInfo(@RequestParam userId: Int): ResponseEntity<Response<List<UserReportDTO>>> {
         val findUserReportInfo = adminPageService.findReportInfoByUserId(userId)
         return ResponseEntity.ok().body(Response.success(findUserReportInfo))
     }
+
     @GetMapping("/login")
     fun loginPage(): String {
         return "admin_login_page"
     }
+
     @PostMapping("/banner/insert")
     fun insertBanner(@RequestBody bannerDTO: BannerDTO): ResponseEntity<ResponseUnit> {
         bannerService.insertBanner(bannerDTO)
         return ResponseEntity.ok(Response.stateOnly(true))
     }
+
     @GetMapping("/banner/find/bannerid")
     fun findBannerByBannerId(@RequestParam bannerId: Int): ResponseEntity<Response<BannerDTO>> {
         val findBannerByBannerId = bannerService.findBannerByBannerId(bannerId)
         return ResponseEntity.ok().body(Response.success(findBannerByBannerId))
     }
+
     @GetMapping("/banner/find/company")
     fun findBannerByCompany(@RequestParam company: String): ResponseEntity<Response<List<BannerDTO>>> {
         val findBannerByCompany = bannerService.findBannerByCompany(company)
         return ResponseEntity.ok().body(Response.success(findBannerByCompany))
     }
+
     @PostMapping("/banner/update/title")
     fun updateTitleByBannerId(@RequestParam bannerId: Int, @RequestParam title: String): ResponseEntity<ResponseUnit> {
         bannerService.updateTitleByBannerId(bannerId, title)
         return ResponseEntity.ok(Response.stateOnly(true))
     }
+
     @PostMapping("/banner/update/imagesurl")
-    fun updateImagesUrlByBannerId(@RequestParam bannerId: Int, @RequestParam imagesUrl: String): ResponseEntity<ResponseUnit> {
+    fun updateImagesUrlByBannerId(
+        @RequestParam bannerId: Int,
+        @RequestParam imagesUrl: String
+    ): ResponseEntity<ResponseUnit> {
         bannerService.updateTitleByBannerId(bannerId, imagesUrl)
         return ResponseEntity.ok(Response.stateOnly(true))
     }
+
     @PostMapping("/banner/update/url")
     fun updateUrlByBannerId(@RequestParam bannerId: Int, @RequestParam url: String): ResponseEntity<ResponseUnit> {
         bannerService.updateTitleByBannerId(bannerId, url)
         return ResponseEntity.ok(Response.stateOnly(true))
     }
+
     @GetMapping("/banner/get")
     fun getBannerByBannerId(@RequestParam bannerId: Int): ResponseEntity<Response<GetBannerDTO>> {
         val getBannerByBannerId = bannerService.getBannerByBannerId(bannerId)
-        return ResponseEntity.ok().body(Response.success(getBannerByBannerId))    }
+        return ResponseEntity.ok().body(Response.success(getBannerByBannerId))
+    }
 }
