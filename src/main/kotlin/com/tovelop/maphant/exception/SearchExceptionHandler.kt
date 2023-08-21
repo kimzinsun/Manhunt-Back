67e49edbeeb00dddf71335e401cbc2805d64b277
Choijake <shameless8@naver.com>
2023-08-21 15:57:37 +0900

    Feat-서치예외처리함수 생성

---
+++ src/main/kotlin/com/tovelop/maphant/exception/SearchExceptionHandler.kt
@@ -0,0 +1,11 @@
+//package com.tovelop.maphant.exception
+//
+//import com.tovelop.maphant.controller.AwsS3Controller
+//import com.tovelop.maphant.controller.SearchController
+//import org.springframework.web.bind.annotation.ExceptionHandler
+//import org.springframework.web.bind.annotation.RestControllerAdvice
+//
+//@RestControllerAdvice(assignableTypes = [SearchController::class])
+//class SearchExceptionHandler {
+//    @ExceptionHandler(value)
+//}
