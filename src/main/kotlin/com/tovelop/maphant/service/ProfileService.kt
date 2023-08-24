339c46c04b3cf543240b9f20cfd774f5694843f2
Choijake <shameless8@naver.com>
2023-08-24 14:46:29 +0900

    프로필 기본이미지 변경

--- src/main/kotlin/com/tovelop/maphant/service/ProfileService.kt
+++ src/main/kotlin/com/tovelop/maphant/service/ProfileService.kt
@@ -21,7 +21,7 @@ class ProfileService(
     private val uploadLogService: UploadLogService,
     private val commentMapper: CommentMapper
 ) {
-    val defaultProfileImg = "https://tovelope.s3.ap-northeast-2.amazonaws.com/image_1.jpg"
+    val defaultProfileImg = "https://tovelope.s3.ap-northeast-2.amazonaws.com/default_profile_img.jpg"
 
     //프로필 이미지의 Dto 불러오기
     fun getNicknameAndBodyAndImage(targetUserId: Int): ProfileCategoryDTO =
